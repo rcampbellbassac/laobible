83d60a187ff8829d379083ece6449668d30f239d
Robert M Campbell <robert.rcampbell@gmail.com>
2019-01-18 23:25:22 +0700

    fill About section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,66 @@
 <template>
   <div class="about">
     <h1>About Us</h1>
+
+    <p>
+      The last iteration of the LaoBible app, crafted from the
+      <a href="http://laobible.net/" target="_blank">
+        LaoBible.net
+        <i class="material-icons">
+          open_in_new
+        </i>
+      </a>
+      content has now come as a sleek, minimal progressive web app. It supports
+      offile use. It's easy to navigte. It's simple. And, it comes as a result of
+      technological and accessbility advances in Lao. Now that Lao can be easily
+      created, accessed, and shared online - the first app to ever published in
+      Lao without using 'invisble spaces', is now being retired. No more
+      development will be performed. It will remain here, as long as it is still
+      used by some.
+    </p>
+
+    <p><strong>Now there are 2 options:</strong></p>
+
+    <p>
+      <ul class="list-group">
+        <li class="list-group-item">
+          The LaoBible Web App - which you are now using
+        </li>
+        <li class="list-group-item">
+          The Lao Revised Version - which you can install
+          <a href="https://play.google.com/store/apps/details?id=org.ubs.lao.rlv"
+          target="_blank">
+          here
+          <i class="material-icons">
+            open_in_new
+          </i>
+        </a>
+      </li>
+    </ul>
+    </p>
+
+    <blockquote class="blockquote text-center">
+      <p class="mb-0">
+        If you lose Heaven, you lose everything;
+        if you gain Heaven, you gain everything.
+      </p>
+      <footer class="blockquote-footer">
+        Ellen Gould White in
+        <cite title="Source Title">Our Father Cares</cite>
+      </footer>
+    </blockquote>
+
+    <p>
+      <small>
+        Photo of the Bible by David Ball: www.davidball.net <br>
+        Licensed <a href="https://creativecommons.org/licenses/by/2.5/deed.en">CC BY 2.5</a>
+      </small>
+    </p>
   </div>
 </template>
+
+<style>
+  .material-icons {
+    font-size: 1rem;
+  }
+</style>
