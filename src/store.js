9778c8b6a881ae986983757eaa2239e73d822d56
Robert M Campbell <robert.rcampbell@gmail.com>
2019-01-18 19:20:12 +0700

    Prep store using BCheck as a template

--- src/store.js
+++ src/store.js
@@ -1,16 +1,41 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import localforage from 'localforage';
+
+localforage.config({
+  name: 'LaoBible',
+});
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    data: {},
+    errors: [],
   },
   mutations: {
-
+    LOAD: (state, initdata) => {
+      state.data = initdata;
+    },
+    ERROR: (state, error) => {
+      state.errors.push(error);
+    },
   },
   actions: {
-
+    async initdata({ commit }) {
+      localforage.getItem('LaoBible').then((localstoragedata) => {
+        if (localstoragedata != null) {
+          commit('LOAD', localstoragedata);
+        } else {
+          try {
+            const initdata = require('@/data/LaoBible.json'); // eslint-disable-line global-require
+            localforage.setItem('LaoBible', initdata).then(commit('LOAD', initdata));
+          } catch (e) {
+            commit('ERROR', { title: 'Failed to load inital data.', error: e });
+          }
+        }
+      });
+    },
   },
 });
