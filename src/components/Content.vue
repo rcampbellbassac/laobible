1362cf6404e49bb5eed1ed8c34ce965bb61b6123
Robert M Campbell <robert.rcampbell@gmail.com>
2019-01-19 00:48:19 +0700

    Content view

---
+++ src/components/Content.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="content">
+    <router-link class="btn btn-primary backbtn"
+    :to="{name: 'chapters', params: {bk: Number(bk)}}">
+      <i class="material-icons">
+        arrow_back
+      </i>
+      Back
+    </router-link>
+    <div class="card">
+      <div class="card-body" v-html="chapter.content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  props: {
+    bk: {
+      type: String,
+      required: true,
+    },
+    ch: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    book() {
+      // Get book information
+      const result = this.$store.state.data.unit.filter(book => book.id == Number(this.bk));
+      return result[0];
+    },
+    chapter() {
+      // Get book information
+      const mybk = Number(this.bk);
+      const mych = Number(this.ch);
+      const book = this.$store.state.data.unit.filter(book => book.id == mybk);
+      if(book.length > 0) {
+        const result = book[0].contents.filter(chapter => chapter.id == mych)
+        return result[0];
+      } else {
+        return {};
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.material-icons {
+  font-size: 1rem;
+}
+
+.backbtn {
+  margin-bottom: 1rem;
+}
+</style>
