d6d76027195784f148d61f9e472d57ad7bc2d9c5
Robert M Campbell <robert.rcampbell@gmail.com>
2019-01-18 23:26:00 +0700

    setup Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="LaoBible Web App"/>
+    <div>
+      <h1 class="text-primary" v-html="name"></h1>
+      <div class="text-secondary" v-html="description"></div>
+    </div>
+    <img class="logo" src="@/assets/bible.jpg"
+    alt="Photo of the Bible by David Ball: www.davidball.net, CC BY 2.5, https://creativecommons.org/licenses/by/2.5/deed.en"/>
+    <div class="action">
+      <router-link class="btn btn-primary" to="/read">ເປີດ / Open</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// @ is an alias to /srcS
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  computed: {
+    name() {
+      return this.$store.state.data.title;
+    },
+    description() {
+      return this.$store.state.data.description;
+    },
   },
 };
 </script>
+
+<style>
+  .logo {
+    max-width: 80%;
+  }
+  .action {
+    margin-top: 2rem;
+  }
+</style>
